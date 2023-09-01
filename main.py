5f9bd1c04e9abf2ccf6c7feb39713d4af2d40106
progod010 <progod.bluezao@gmail.com>
2023-09-01 19:52:47 -0300

    teste124265

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
-n1 = input(int('Digite um número: '))
-n2 = input(int('Digite outro número: '))
-n3 = (n1+n2)
-print(f'O resultado da soma de {n1} e {n2} é igual a {n3}.')
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print(f'O resultado da soma de {n1} e {n2} é igual a {(n1+n2)}.')
 time.sleep(7)
