958ebd756884172ac341a55952bdae6f2986f8d5
progodzitos <130160814+progodzitos@users.noreply.github.com>
2023-09-01 19:26:12 -0300

    Create main.py
    criei um arquivinho só de exemplo,  ja apago

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+n1 = input(int('Digite um número: '))
+n2 = input(int('Digite outro número: '))
+n3 = (n1+n2)
+print(f'O resultado da soma de {n1} e {n2} é igual a {n3}.')
+time.sleep(7)
